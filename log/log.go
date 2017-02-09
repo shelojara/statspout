a0efa73ecaf49d9dbb0081a638c109d3b1681b74
mijara <marjara35@gmail.com>
2017-02-09 12:45:38 -0300

    Add warning level

--- log/log.go
+++ log/log.go
@@ -6,13 +6,15 @@ import (
 )
 
 var (
-	Info  *log.Logger
-	Error *log.Logger
-	Debug *log.Logger
+	Info    *log.Logger
+	Error   *log.Logger
+	Debug   *log.Logger
+	Warning *log.Logger
 )
 
 func init() {
-	Info = log.New(os.Stdout, "INFO: ", log.LstdFlags|log.Lmicroseconds)
-	Error = log.New(os.Stderr, "ERROR: ", log.LstdFlags|log.Lmicroseconds)
-	Debug = log.New(os.Stdout, "DEBUG: ", log.LstdFlags|log.Lmicroseconds)
+	Info    = log.New(os.Stdout, "INFO: ", log.LstdFlags|log.Lmicroseconds)
+	Error   = log.New(os.Stderr, "ERROR: ", log.LstdFlags|log.Lmicroseconds)
+	Debug   = log.New(os.Stdout, "DEBUG: ", log.LstdFlags|log.Lmicroseconds)
+	Warning = log.New(os.Stdout, "WARNING: ", log.LstdFlags|log.Lmicroseconds)
 }
